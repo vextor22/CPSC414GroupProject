8e1c7d2e7051a1c0f6b5eb91730a77304edcbb30
Matt Higgins <mrhiggi@g.clemson.edu>
2017-03-26 12:18:12 -0400

    Created project structure with launching class and a first JFrame

---
+++ src/prototypeAR/KitchenARPrototype.java
@@ -0,0 +1,11 @@
+package prototypeAR;
+
+public class KitchenARPrototype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			MainScreen mainScreen = new MainScreen();
+			mainScreen.setVisible(true);
+	}
+
+}
--- src/prototypeAR/MainScreen.java
+++ src/prototypeAR/MainScreen.java
@@ -1,10 +1,24 @@
 package prototypeAR;
 
-public class MainScreen {
+import java.awt.BorderLayout;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class MainScreen extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6737387360403996561L;
+
+	public MainScreen(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Kitchen AR Prototype");
+		JLabel label = new JLabel("First Item");
+		getContentPane().add(label, BorderLayout.CENTER);
+		pack();
+		
 	}
 
 }
